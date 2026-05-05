158634bf70cc6b25eb37637bbbccfecd8a3dec97
JustDrven <petr.nemec11111@gmail.com>
2026-05-05 08:17:35 +0200

    chore: improve naming and apply constants from pkg folder

--- internal/orm/index.go
+++ internal/orm/index.go
@@ -2,36 +2,31 @@ package orm
 
 import (
 	"fmt"
-	"time"
 
 	"dev.justdrven/loadbalancer/internal/config"
 	"dev.justdrven/loadbalancer/internal/service"
+	"dev.justdrven/loadbalancer/pkg"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-const (
-	CONNECTION_STRING_DEFAULT string        = "%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local"
-	TRANS_TIMEOUT             time.Duration = 5 * time.Second
-)
-
-func OrmInit(configData config.Config) *gorm.DB {
+func Initialize(configData config.Config) (*gorm.DB, error) {
 	fmt.Println("[ORM-MANAGER] Initializing..")
 
 	connectinString := createConnectionString(configData)
 
 	db, err := gorm.Open(mysql.Open(connectinString), &gorm.Config{
-		DefaultTransactionTimeout: TRANS_TIMEOUT,
+		DefaultTransactionTimeout: pkg.TRANS_TIMEOUT,
 	})
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	fmt.Println("[ORM-MANAGER] Starting configuration of DB")
 	db.AutoMigrate(&service.Service{})
 
 	fmt.Println("[ORM-MANAGER] Done")
-	return db
+	return db, nil
 }
 
 func createConnectionString(cnf config.Config) string {
@@ -40,5 +35,5 @@ func createConnectionString(cnf config.Config) string {
 	password := cnf.Password
 	dbname := cnf.DbName
 
-	return fmt.Sprintf(CONNECTION_STRING_DEFAULT, username, password, addr, dbname)
+	return fmt.Sprintf(pkg.CONNECTION_STRING_DEFAULT, username, password, addr, dbname)
 }
